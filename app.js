b0b32810ed9001614f91c20cfc200976c7023290
Marc Müller <for.maex@bluemail.ch>
2015-03-05 13:12:44 +0100

    Module GEvents created
    Modul now exposes the calendar.events.list function. Connection is
    doen with an API_KEY as the calendar is pblicly available.
    
    ToDo: some caching to decrease the number of requests and as such
    the speed with which the data can be served.

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var stammtreffs = require('./routes/stammtreffs')
 
 var app = express();
 
@@ -15,8 +16,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -25,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/stammtreffs', stammtreffs);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
